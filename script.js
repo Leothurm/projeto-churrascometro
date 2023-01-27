0e272cfb6f134fa6d1ad6db19b7913cf2c7acc34
Leothurm <leonardo.thurm@gmail.com>
2023-01-27 20:52:08 -0300

    parte 2 - Continuação JavaScript

--- script.js
+++ script.js
@@ -12,16 +12,34 @@ function calcular(){
     let duracao = inputDuracao.value;
 
     let qtdCarne = carnePP(duracao)* adultos + carnePP(duracao)*criancas/2
+    let qtdCerveja = cervejaPP(duracao)*adultos;
+    let qtdBebidas = bebidaPP(duracao)*adultos + bebidaPP(duracao)*criancas/2
 
-
-    console.log(qtdCarne)
+    resultado.innerHTML = `<p>${qtdCarne/1000} kg de carne </p>`
+    resultado.innerHTML += `<p>${Math.ceil(qtdCerveja/355)} latas de cerveja </p>`
+    resultado.innerHTML += `<p>${Math.ceil(qtdBebidas/2000)} garrafas de refri </p>`
+   
 }
 
 function carnePP(duracao){
-    let carne = 400;
     if(duracao>=6){
-        return 650
+        return 650;
     } else{
-        return 400
+        return 400;
+    }
+}
+
+function cervejaPP(duracao){
+    if(duracao>=6){
+        return 2000;
+    } else{
+        return 1200;
+    }
+}
+function bebidaPP(duracao){
+    if(duracao>=6){
+        return 1500;
+    } else{
+        return 1000;
     }
 }
